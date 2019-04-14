76e45774b2410e645de3ac05deec61aa180ef02a
Ivy Strickland <IvyStrickland@pursuit.org>
2019-04-14 18:15:45 -0400

    get ratings by id

--- backend /services/ratings.js
+++ backend /services/ratings.js
@@ -9,3 +9,12 @@ RatingService.readAll = () => {
     `;
     return db.any(sql);
 };
+RatingService.read = (id) => {
+    const sql = `
+    SELECT *
+    FROM ratings
+    WHERE
+    ratings.id = $[id]  
+    `;
+    return db.any(sql, {id});
+};
