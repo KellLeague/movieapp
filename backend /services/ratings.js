01aaed8648910fe068d051293d60c4e8d10de15a
Ivy Strickland <IvyStrickland@pursuit.org>
2019-04-14 18:15:15 -0400

    read all ratings

--- backend /services/ratings.js
+++ backend /services/ratings.js
@@ -1,2 +1,11 @@
 const db = require('./dbConnect');
 const RatingService = {};
+
+//READ
+RatingService.readAll = () => {
+    const sql = `
+    SELECT *
+    FROM ratings  
+    `;
+    return db.any(sql);
+};
