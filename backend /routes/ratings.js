9b3f3dea401671b7a11983924d4a308fca637755
Ivy Strickland <IvyStrickland@pursuit.org>
2019-04-14 17:54:17 -0400

    get all ratings

--- backend /routes/ratings.js
+++ backend /routes/ratings.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const ratingRouter = express.Router();
 const RatingService = require('../services/ratings');
+
+// Get rating info 
+ratingRouter.get('/all', (req, res, next)=>{
+    RatingService.readAll()
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        next(err);
+    });
+});
