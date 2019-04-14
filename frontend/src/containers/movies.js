6feee07248e1860985d34784cc84b1a35b18b988
Ivy Strickland <IvyStrickland@pursuit.org>
2019-04-14 10:40:09 -0400

    added movies

---
+++ frontend/src/containers/movies.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Movies extends React.Component{
+constructor(props){
+    super(props)
+    
+    this.state = {
+        movies:'',
+        ratings:0,
+        image:''
+    }
+    
+}
+        render(){
+        return (
+  
+        )
+   
+    
+  } 
+}
+export default Movies;
