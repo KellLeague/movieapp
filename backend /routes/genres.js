7e3322ec94ad7fce815e8f6c879858ac887a88c9
Ivy Strickland <IvyStrickland@pursuit.org>
2019-04-14 17:57:59 -0400

    get get genres

--- backend /routes/genres.js
+++ backend /routes/genres.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const genreRouter = express.Router();
 const genreService = require('../services/genres');
+
+
+// GET GENRE
+genreRouter.get('/', (req, res, next) => {
+    
+    genreService.read()
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        next(err);
+    });
+});
