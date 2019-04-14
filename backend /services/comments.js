a24c628164c4d709a669489d3a1b9b531b0d2bdd
Ivy Strickland <IvyStrickland@pursuit.org>
2019-04-14 18:26:04 -0400

    read all comments

--- backend /services/comments.js
+++ backend /services/comments.js
@@ -1,2 +1,12 @@
 const db = require('./dbConnect');
 const CommentService = {};
+
+//READ
+
+CommentService.readAll = () => {
+    const sql = `
+    SELECT *
+    FROM comments
+    `;
+    return db.any(sql);
+};
