8ce0705c6021229b4a890ca39285ec6e4037ef93
Ivy Strickland <IvyStrickland@pursuit.org>
2019-04-14 18:24:07 -0400

    read genres

--- backend /services/genres.js
+++ backend /services/genres.js
@@ -1,2 +1,11 @@
 const db = require('./dbConnect');
 const genreService = {};
+
+//READ
+genreService.read = () => {
+    const sql = `
+    SELECT *
+    FROM genres
+    `;
+    return db.any(sql);
+};
