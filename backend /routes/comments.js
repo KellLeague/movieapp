0a83c41bf27930b27960f4e43adeaade541235b1
Ivy Strickland <IvyStrickland@pursuit.org>
2019-04-14 18:01:05 -0400

    get all comments

--- backend /routes/comments.js
+++ backend /routes/comments.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const commentRouter = express.Router();
 const CommentService = require('../services/comments');
+
+//GET COMMENT INFO
+commentRouter.get('/all', (req, res, next)=> {
+    CommentService.readAll()
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => {
+        next(err);
+    });
+});
