a75a548b49c145af3d12145ea531bbf4b7dddf5d
Ivy Strickland <IvyStrickland@pursuit.org>
2019-04-15 14:00:47 -0400

    added input event

--- frontend/src/containers/genres.js
+++ frontend/src/containers/genres.js
@@ -12,14 +12,18 @@ class  Genres extends React.Component {
     }
 
 
-    // handleInput (e) 
+    handleInput = (e) => {
+        this.setState ({input: e.target.value})
+    } 
 
     render(){
+        console.log(this.state);
+
     return(
         <form >
         <div className='container' >
             <div className='input-group mb-3' >
-          <input type="text" class="form-control" placeholder="Movie Genre" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+          <input type="text" onChange={this.handleInput} class="form-control" placeholder="Movie Genre" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
          <div className='input-group-append' >
                        <button class="btn btn-outline-secondary" type="button">Search by Genre</button>
          </div>
